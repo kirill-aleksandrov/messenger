a0dee264b71a0bcb9b0712fc9f1a2b036f7d5f01
Kirill Aleksandrov <xakepgame@gmail.com>
2020-03-08 23:52:01 +0700

    Connect to database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,23 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import { createConnection } from 'typeorm';
+import { entities } from './entities';
 
-const app = express();
+dotenv.config();
 
-app.get('/', (req, res) => res.send('Hello world'));
+createConnection({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    entities,
+    synchronize: true,
+}).then(() => {
+    const app = express();
 
-app.listen(3000, () => console.log('Running'));
+    app.get('/', (req, res) => res.send('Hello world'));
+
+    app.listen(3000, () => console.log('Running'));
+}).catch(console.log);
