edd7c66b97bd70a6b9ca5ef897eb1aba68e88364
LoKi1237 <loki1237@yandex.ru>
2020-06-19 15:08:43 +0300

    Configure css modules

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.m.css';
+
+const App = () => {
+    return (
+	    <div className={styles.App}>
+            <h1>Hello world</h1>
+        </div>
+    );
+}
+
+export default App;
